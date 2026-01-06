5e52b19792268447e464315065f23c18637aceea
Charlie-Boots <charles.zhangyichi@gmail.com>
2026-01-06 20:46:13 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,130 @@
+namespace SpriteKind {
+    export const npc = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc, function (sprite, otherSprite) {
+    mySprite2.sayText("click B to talk", 500, false)
+    if (controller.B.isPressed()) {
+        game.showLongText("hi boots！I'm strong monkey，here is the forest", DialogLayout.Bottom)
+        game.showLongText("I can let you go but be careful", DialogLayout.Bottom)
+        tiles.placeOnTile(mySprite2, tiles.getTileLocation(10, 11))
+        tiles.setWallAt(tiles.getTileLocation(9, 12), false)
+    }
+    tiles.setWallAt(tiles.getTileLocation(9, 12), false)
+    effects.clearParticles(mySprite2)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile18`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`级别2`)
+    mySprite2 = sprites.create(img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . f d f d d f d e e f . . . . . 
+        . f d f d d f d e e f f . . . . 
+        f d e e d d d d e e d d f . . . 
+        f d d d d d d d e e b d c . . . 
+        f c c c c c d e e e b d c . f f 
+        . f d d d d e e e f f c . f e f 
+        . f f f f f f e e e e f . f e f 
+        . f f f f e e e e e e e f f e f 
+        f d d f d d f e f e e e e f f . 
+        f d b f d b f e f e e e e f . . 
+        f f f f f f f f f f f f e f . . 
+        . f e e f e e f . f e e e f . . 
+        . f e e f e e f . . f f f f . . 
+        `, SpriteKind.npc)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 7))
+    tiles.placeOnTile(mySprite2, tiles.getTileLocation(9, 11))
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile20`, function (sprite, location) {
+    sprites.destroy(mySprite2)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 1))
+    mySprite.sayText("looks like this is the forest", 2000, false)
+    tiles.setCurrentTilemap(tilemap`级别3`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`级别0`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(9, 14))
+    mySprite2 = sprites.create(img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . f d f d d f d e e f . . . . . 
+        . f d f d d f d e e f f . . . . 
+        f d e e d d d d e e d d f . . . 
+        f d d d d d d d e e b d c . . . 
+        f c c c c c d e e e b d c . f f 
+        . f d d d d e e e f f c . f e f 
+        . f f f f f f e e e e f . f e f 
+        . f f f f e e e e e e e f f e f 
+        f d d f d d f e f e e e e f f . 
+        f d b f d b f e f e e e e f . . 
+        f f f f f f f f f f f f e f . . 
+        . f e e f e e f . f e e e f . . 
+        . f e e f e e f . . f f f f . . 
+        `, SpriteKind.npc)
+    tiles.placeOnTile(mySprite2, tiles.getTileLocation(10, 11))
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`级别1`)
+mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+forever(function () {
+    if (mySprite.vx > 0) {
+        mySprite.setImage(img`
+            . . . . . . . f f f f f . . . . 
+            . . . . . . f e e e e e f . . . 
+            . . . . . f e e e d d d d f . . 
+            . . . . f f e e d f d d f d c . 
+            . . . f d d e e d f d d f d c . 
+            . . . c d b e e d d d d e e d c 
+            f f . c d b e e d d c d d d d c 
+            f e f . c f e e d d d c c c c c 
+            f e f . . f f e e d d d d d f . 
+            f e f . f e e e e f f f f f . . 
+            f e f f e e e e e e e f . . . . 
+            . f f e e e e f e f f e f . . . 
+            . . f e e e e f e f f e f . . . 
+            . . . f e f f b d f b d f . . . 
+            . . . f d b b d d c d d f . . . 
+            . . . f f f f f f f f f . . . . 
+            `)
+    } else if (mySprite.vx < 0) {
+        mySprite.setImage(img`
+            . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f . . . . . . 
+            . . f d d d d e e e f . . . . . 
+            . c d f d d f d e e f f . . . . 
+            . c d f d d f d e e d d f . . . 
+            c d e e d d d d e e b d c . . . 
+            c d d d d c d d e e b d c . f f 
+            c c c c c d d d e e f c . f e f 
+            . f d d d d d e e f f . . f e f 
+            . . f f f f f e e e e f . f e f 
+            . . . . f e e e e e e e f f e f 
+            . . . f e f f e f e e e e f f . 
+            . . . f e f f e f e e e e f . . 
+            . . . f d b f d b f f e f . . . 
+            . . . f d d c d d b b d f . . . 
+            . . . . f f f f f f f f f . . . 
+            `)
+    }
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,192 @@
+// 自动生成的代码。请勿编辑。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile23 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile24 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "级别1":
+            case "级别1":return tiles.createTilemap(hex`1000100003030303030303030303030303030303030303030303030303030303030303030303070404040404040404040404060303030201010101010101010101010603030302010e0b0b0b0b0b0b0b0f010503030302010d0c0c0a0a0a0c0c1001050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101090a0801010101050303030201010101091108010101010503`, img`
+. . . . . . . . . . . . . . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . 2 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile14,myTiles.tile15,myTiles.tile16,myTiles.tile17,myTiles.tile18,myTiles.tile19,myTiles.tile20], TileScale.Sixteen);
+            case "级别2":
+            case "级别2":return tiles.createTilemap(hex`1000100003030303030303030404030303030303030202020202020202020202020202030302030303020202020302030203020303020203020203030203020303020203030202030203070302030203030202030302020302020303020302030203020303020202020202020202020202020203030203030303020202020303020202030302020202020202020202020202020106020302020202030302020202020203030202020202020203020202020202030302020202020203020202020202020303020202020202030302030303020203030202020202020303020303020202030302020203030303020202030202030303020202030202020505030303030303`, img`
+2 2 2 2 2 2 2 2 . . 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 2 2 . . . . 2 . 2 . 2 . 2 
+2 . . 2 . . 2 2 . 2 . 2 2 . . 2 
+2 . . 2 . . . 2 . 2 . 2 2 . . 2 
+2 . . 2 . . 2 2 . 2 . 2 . 2 . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 2 2 2 . . . . 2 2 . . . 2 
+2 . . . . . . . . . . . . . . . 
+. . 2 . . . . 2 2 . . . . . . 2 
+2 . . . . . . . 2 . . . . . . 2 
+2 . . . . . . 2 . . . . . . . 2 
+2 . . . . . . 2 2 2 2 2 2 . . 2 
+2 . . . . . . 2 2 . 2 2 . . . 2 
+2 . . . 2 2 2 2 . . . 2 . . 2 2 
+2 . . . 2 . . . . . 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,myTiles.tile21,myTiles.tile22,myTiles.tile23,myTiles.tile24], TileScale.Sixteen);
+            case "级别3":
+            case "级别3":return tiles.createTilemap(hex`1000100004040404040401010404040404040404040202020202020202020202020202040404020204020202040202020202040404020202020202020202020402020204040202020202020202020202020202040402020202020204020202040202040404020204020204020202020202020204040402020202020202020402020202040402020402040204020202020202020404020202020202020202020202020204040402020202020204020202040202040402020202040202020202020402020404020402020202020204020202020204040202020202020204020202020204040404020202020202020202040202020404040404040403030404040404040404`, img`
+2 2 2 2 2 2 . . 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 . . 2 . . . 2 . . . . . 2 2 
+2 . . . . . . . . . . 2 . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . 2 . . . 2 . . 2 2 
+2 . . 2 . . 2 . . . . . . . . 2 
+2 2 . . . . . . . . 2 . . . . 2 
+2 . . 2 . 2 . 2 . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 . . . . . . 2 . . . 2 . . 2 
+2 . . . . 2 . . . . . . 2 . . 2 
+2 . 2 . . . . . . 2 . . . . . 2 
+2 . . . . . . . 2 . . . . . 2 2 
+2 2 . . . . . . . . . 2 . . . 2 
+2 2 2 2 2 2 . . 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass1,sprites.builtin.forestTiles0], TileScale.Sixteen);
+            case "级别0":
+            case "级别4":return tiles.createTilemap(hex`1000100003030303030303030404030303030303030202020202020202020202020202030302030303020202020302030203020303020203020203030203020303020203030202030203070302030203030202030302020302020303020302030203020303020202020202020202020202020203030203030303020202020303020202030302020202020202020202020202020106020302020202030302020202020203030202020202020203020202020202030302020202020203020202020202020303020202020202030302030303020203030202020202020303020303020202030302020203030303020202030202030303020202030202020505030303030303`, img`
+2 2 2 2 2 2 2 2 . . 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 2 2 . . . . 2 . 2 . 2 . 2 
+2 . . 2 . . 2 2 . 2 . 2 2 . . 2 
+2 . . 2 . . . 2 . 2 . 2 2 . . 2 
+2 . . 2 . . 2 2 . 2 . 2 . 2 . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 2 2 2 . . . . 2 2 . . . 2 
+2 . . . . . . . . . . . . . . . 
+. . 2 . . . . 2 2 . . . . . . 2 
+2 . . . . . . . 2 . . . . . . 2 
+2 . . . . . . 2 . . . . . . . 2 
+2 . . . . . . 2 2 . 2 2 2 . . 2 
+2 . . . . . . 2 2 . 2 2 . . . 2 
+2 . . . 2 2 2 2 . . . 2 . . 2 2 
+2 . . . 2 . . . . . 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,myTiles.tile21,myTiles.tile22,myTiles.tile23,myTiles.tile24], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile15":
+            case "tile17":return tile17;
+            case "myTile16":
+            case "tile18":return tile18;
+            case "myTile17":
+            case "tile19":return tile19;
+            case "myTile18":
+            case "tile20":return tile20;
+            case "myTile19":
+            case "tile21":return tile21;
+            case "myTile20":
+            case "tile22":return tile22;
+            case "myTile21":
+            case "tile23":return tile23;
+            case "myTile22":
+            case "tile24":return tile24;
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
